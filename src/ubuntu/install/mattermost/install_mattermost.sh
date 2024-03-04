01928f4e9cb4c9716af82cef261e85fad609c00e
Bart <bartekj@users.noreply.github.com>
2024-03-04 11:09:43 +0100

    Add more tools, fix mattermost shortcut and add cleaning

--- src/ubuntu/install/mattermost/install_mattermost.sh
+++ src/ubuntu/install/mattermost/install_mattermost.sh
@@ -4,3 +4,19 @@ set -ex
 curl -o- https://deb.packages.mattermost.com/setup-repo.sh | sudo bash
 
 sudo apt-get install mattermost-desktop
+
+sed -i 's,/opt/Mattermost/mattermost-desktop,/opt/Mattermost/mattermost-desktop --no-sandbox,g' /usr/share/applications/mattermost-desktop.desktop
+cp /usr/share/applications/mattermost-desktop.desktop $HOME/Desktop/ 
+chmod +x $HOME/Desktop/mattermost-desktop.desktop
+chown 1000:1000 $HOME/Desktop/mattermost-desktop.desktop
+
+# Cleanup for app layer
+chown -R 1000:0 $HOME
+find /usr/share/ -name "icon-theme.cache" -exec rm -f {} \;
+if [ -z ${SKIP_CLEAN+x} ]; then
+  apt-get autoclean
+  rm -rf \
+    /var/lib/apt/lists/* \
+    /var/tmp/* \
+    /tmp/*
+fi
--- src/ubuntu/install/obsidian/install_obsidian.sh
+++ src/ubuntu/install/obsidian/install_obsidian.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 set -ex
+
 curl -Lso obsidian.deb https://github.com/obsidianmd/obsidian-releases/releases/download/v1.5.8/obsidian_1.5.8_amd64.deb
 apt install -y ./obsidian.deb
 sed -i 's#/opt/Obsidian/obsidian#/opt/Obsidian/obsidian --no-sandbox##' /usr/share/applications/obsidian.desktop
@@ -7,3 +8,14 @@ cp /usr/share/applications/obsidian.desktop $HOME/Desktop
 chmod +x $HOME/Desktop/obsidian.desktop
 chown 1000:1000 $HOME/Desktop/obsidian.desktop
 rm obsidian.deb
+
+# Cleanup for app layer
+chown -R 1000:0 $HOME
+find /usr/share/ -name "icon-theme.cache" -exec rm -f {} \;
+if [ -z ${SKIP_CLEAN+x} ]; then
+  apt-get autoclean
+  rm -rf \
+    /var/lib/apt/lists/* \
+    /var/tmp/* \
+    /tmp/*
+fi
