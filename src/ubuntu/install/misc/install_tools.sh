75d7dd964f7c113ec7d0ee461cb8f31774e00fdb
Bart <bartekj@users.noreply.github.com>
2024-05-15 14:31:59 +0200

    Update install_tools.sh

--- src/ubuntu/install/misc/install_tools.sh
+++ src/ubuntu/install/misc/install_tools.sh
@@ -7,5 +7,5 @@ if [ "$DISTRO" = centos ]; then
   yum install xdotool -y
 else
   apt-get update
-  apt-get install -y nano zip xdotool nmap screen iputils-ping mtr irssi cifs-utils gvfs-backends rar unrar jq 
+  apt-get install -y nano zip xdotool nmap screen iputils-ping mtr irssi cifs-utils gvfs-backends jq kitty kitty-shell-integration kitty-terminfo 
 fi
