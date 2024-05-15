a3f5b2f0ea4401841f6a8f28c8c9a4417d03e288
Bart <bartekj@users.noreply.github.com>
2024-05-15 12:53:10 +0200

    Update install_terraform.sh

--- src/ubuntu/install/terraform/install_terraform.sh
+++ src/ubuntu/install/terraform/install_terraform.sh
@@ -8,7 +8,7 @@ if [ "${ARCH}" == "arm64" ] ; then
     exit 0
 fi
 
-apt-get install -y apt-add-repository
+apt-get install -y software-properties-common
 
 curl -fsSL https://apt.releases.hashicorp.com/gpg | apt-key add -
 apt-add-repository "deb [arch=$(dpkg --print-architecture)] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
